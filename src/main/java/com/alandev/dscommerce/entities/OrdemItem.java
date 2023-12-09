0787564eff56430ecf8bebfdf657090fce525443
Allan-Borges <alanborges195@gmail.com>
2023-12-09 05:57:56 -0300

    Equals and hashcode

--- src/main/java/com/alandev/dscommerce/entities/OrdemItem.java
+++ src/main/java/com/alandev/dscommerce/entities/OrdemItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order_item")
 public class OrdemItem {
@@ -54,5 +56,18 @@ public class OrdemItem {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrdemItem ordemItem = (OrdemItem) o;
+        return Objects.equals(id, ordemItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
