a3bce94765345c5760fad129254c35f6e1e47e19
Allan-Borges <alanborges195@gmail.com>
2023-12-11 06:31:23 -0300

    Get products page

--- src/main/java/com/alandev/dscommerce/services/ProductService.java
+++ src/main/java/com/alandev/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.alandev.dscommerce.dto.ProductDTO;
 import com.alandev.dscommerce.entities.Product;
 import com.alandev.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,9 +20,19 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
+        //busca um produto no banco de dados
         Optional<Product> result = productRepository.findById(id);
+        //pega o produto dentro do optional
         Product product = result.get();
+        //copia os dados do produyo obtido para um dto
         ProductDTO dto = new ProductDTO(product);
+        //retorna o dto
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(product -> new ProductDTO(product));
     }
 }
